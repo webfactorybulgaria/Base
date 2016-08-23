529136e282d0b23270eb42d46ce4644ee78c737a
Nasko <nasko@webfactory.bg>
2016-08-23 09:43:39 +0300

    position field

---
+++ resources/assets/typicms/components/list/directives/typiStrToNumber.js
@@ -0,0 +1,13 @@
+angular.module('typicms').directive('stringToNumber', function() {
+    return {
+        require: 'ngModel',
+        link: function(scope, element, attrs, ngModel) {
+            ngModel.$parsers.push(function(value) {
+                return '' + value;
+            });
+            ngModel.$formatters.push(function(value) {
+                return parseFloat(value);
+            });
+        }
+    };
+})
